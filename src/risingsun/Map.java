52ac0120ea3cbcc148e2bc32fab53b76a956bda8
Muchachos <muchachosblablabla@yopmail.com>
2019-03-30 19:33:10 +0100

    THE FINAL > ADA

---
+++ src/risingsun/Map.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package risingsun;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author QB
+ */
+public class Map {
+    Vector<Node> v;
+    public Map() {
+        this.v = new Vector<>();
+    }
+}
