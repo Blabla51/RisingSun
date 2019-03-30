296c12af074b94ac035ea19d9bea295df9471f78
ardayigit <ardayigit@10.43.1.187>
2019-03-30 18:35:11 +0100

    game server still dev

---
+++ src/risingsun/Node.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package risingsun;
+
+/**
+ *
+ * @author ardayigit
+ */
+public class Node {
+    
+    public int id; 
+    public double coordX; 
+    public double coordY; 
+    public int production; 
+    public int qtCode; 
+    public NeighborNode[] neighbors; 
+    public boolean bonus; 
+    public int typeBonus; 
+    public boolean isServer; 
+    public int owner; 
+
+    public Node(int id, double coordX, double coordY, int production, int qtCode, NeighborNode[] neighbors, boolean bonus, int typeBonus, boolean isServer, int owner) {
+        this.id = id;
+        this.coordX = coordX;
+        this.coordY = coordY;
+        this.production = production;
+        this.qtCode = qtCode;
+        this.neighbors = neighbors;
+        this.bonus = bonus;
+        this.typeBonus = typeBonus;
+        this.isServer = isServer;
+        this.owner = owner;
+    }
+    
+    
+    
+}
