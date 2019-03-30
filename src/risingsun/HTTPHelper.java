981032f2ef50c456901b42ec51d29089f01cac48
ardayigit <ardayigit@10.43.1.187>
2019-03-30 16:59:11 +0100

    join ok bis

--- src/risingsun/HTTPHelper.java
+++ src/risingsun/HTTPHelper.java
@@ -6,6 +6,7 @@
 package risingsun;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -43,5 +44,38 @@ public class HTTPHelper {
 		
 	}
 
-    
+    	public static String[] post(String url, String param) throws Exception {
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		con.setRequestMethod("POST");
+                
+                String urlParameters = param;
+                
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(urlParameters);
+                wr.flush();
+                wr.close();
+
+		int responseCode = con.getResponseCode();
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		String ar[] = new String[2];
+		
+		ar[0] = Integer.toString(con.getResponseCode());
+        ar[1] = response.toString();
+		
+		return ar;
+		
+	}    
 }
