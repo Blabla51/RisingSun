075d53ecc438088d8606b3e1740cc16e4107017e
Muchachos <muchachosblablabla@yopmail.com>
2019-03-30 20:19:36 +0100

    Map

--- src/risingsun/Map.java
+++ src/risingsun/Map.java
@@ -16,4 +16,18 @@ public class Map {
     public Map() {
         this.v = new Vector<>();
     }
+    Map prediction(){
+        Map m = new Map();
+        return m;
+    }
+    int getProduction(int id){
+        int p = 0;
+        for (int i = 0; i < v.size(); i++) {
+            if(v.get(i).id == id){
+                p += v.get(i).production;
+            }
+        }
+        return p;
+    }
+    
 }
